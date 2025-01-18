1794a23059fa527affeb0319fbbbdbaa002c2f0e
net-cyber <devnatnaels@gmail.com>
2025-01-18 13:25:16 +0300

    Refactor Makefile and add test target
    - Updated Makefile to include a new target 'test' for running tests with verbose output and code coverage.
    - Added 'go test' command to the 'test' target in Makefile to execute tests for the entire project.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,28 +3,30 @@ package db
 import (
 	"context"
 	"testing"
+
+	"github.com/net-cyber/neka_pay/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := createAccountParams{
-		Owner:    "nati",
-		Balance:  1000,
-		Currency: "ETB",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.createAccount(context.Background(), arg)
 	// check if the error is nil
 	require.NoError(t, err)
 	// check if the account is not empty
 	require.NotEmpty(t, account)
-	
+
 	// check if the account owner is the same as the one we passed
 	require.Equal(t, arg.Owner, account.Owner)
 	// check if the account balance is the same as the one we passed
 	require.Equal(t, arg.Balance, account.Balance)
 	// check if the account currency is the same as the one we passed
 	require.Equal(t, arg.Currency, account.Currency)
-	
+
 	// check if the account id is not zero
 	require.NotZero(t, account.ID)
 	// check if the account created at is not zero
---
+++ util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"ETB"}
+	return currencies[rand.Intn(len(currencies))]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
+}
