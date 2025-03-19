575955f7f116677535a521f4a5a7af25b7c46400
net-cyber <devnatnaels@gmail.com>
2025-03-19 10:37:48 +0300

    Enhance API and configuration for user and account management
    - Added symmetric key and access token duration to app configuration.
    - Updated server initialization to include token management.
    - Refactored account creation to associate accounts with authenticated users.
    - Improved account listing to filter by owner.
    - Enhanced error handling and logging for user and account operations.
    - Updated SQL queries to reflect changes in account management logic.

--- api/account.go
+++ api/account.go
@@ -1,15 +1,15 @@
 package api
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/net-cyber/neka_pay/db/sqlc"
+	"github.com/net-cyber/neka_pay/token"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -20,14 +20,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
 
 	account, err := server.store.CreateAccount(ctx, arg)
-
 	if err != nil {
 		errCode := db.ErrorCode(err)
 		if errCode == db.ForeignKeyViolation || errCode == db.UniqueViolation {
@@ -38,13 +38,11 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
-
-
 	ctx.JSON(http.StatusOK, account)
 }
 
 type getAccountRequest struct {
-	ID int64 `uri:"id" binding:"required"`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -55,9 +53,20 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	}
 
 	account, err := server.store.GetAccount(ctx, req.ID)
-
 	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
 		return
 	}
 
@@ -65,80 +74,29 @@ func (server *Server) getAccount(ctx *gin.Context) {
 }
 
 type listAccountRequest struct {
-	PageID   int32 `form:"page_id" binding:"required"`
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
-func (server *Server) listAccount(ctx *gin.Context) {
+func (server *Server) listAccounts(ctx *gin.Context) {
 	var req listAccountRequest
 	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
 
 	accounts, err := server.store.ListAccounts(ctx, arg)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	fmt.Println("account list", accounts)
 	ctx.JSON(http.StatusOK, accounts)
-}
-
-type updateAccountRequestJSON struct {
-	Balance int64 `json:"balance" binding:"required,min=0"`
-}
-
-func (server *Server) updateAccount(ctx *gin.Context) {
-	var reqUri getAccountRequest
-	if err := ctx.ShouldBindUri(&reqUri); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	var reqJSON updateAccountRequestJSON
-	if err := ctx.ShouldBindJSON(&reqJSON); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	arg := db.UpdateAccountParams{
-		ID:      reqUri.ID,
-		Balance: reqJSON.Balance,
-	}
-
-	account, err := server.store.UpdateAccount(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, account)
-}
-
-type deleteAccountRequest struct {
-	ID int64 `uri:"id" binding:"required"`
-}
-
-func (server *Server) deleteAccount(ctx *gin.Context) {
-	var req deleteAccountRequest
-	if err := ctx.ShouldBindUri(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	err := server.store.DeleteAccount(ctx, req.ID)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{"message": "Account deleted successfully"})
 }
--- api/user.go
+++ api/user.go
@@ -2,61 +2,69 @@ package api
 
 import (
 	// "errors"
+	"errors"
 	"net/http"
 	"time"
-	db "github.com/net-cyber/neka_pay/db/sqlc"
+
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	db "github.com/net-cyber/neka_pay/db/sqlc"
+
 	// "github.com/google/uuid"
 	"github.com/net-cyber/neka_pay/util"
+	"github.com/sirupsen/logrus"
 )
 
 type createUserRequest struct {
-	Username string `json:"username" binding:"required,alphanum"`
-	Password string `json:"password" binding:"required,min=6"`
-	FullName string `json:"full_name" binding:"required"`
-	International_phone_number    string `json:"international_phone_number" binding:"required"`
+	Username                   string `json:"username" binding:"required,alphanum"`
+	Password                   string `json:"password" binding:"required,min=6"`
+	FullName                   string `json:"full_name" binding:"required"`
+	International_phone_number string `json:"international_phone_number" binding:"required"`
 }
 
 type userResponse struct {
-	Username          string    `json:"username"`
-	FullName          string    `json:"full_name"`
-	International_phone_number             string    `json:"international_phone_number"`
-	PasswordChangedAt time.Time `json:"password_changed_at"`
-	CreatedAt         time.Time `json:"created_at"`
+	Username                   string    `json:"username"`
+	FullName                   string    `json:"full_name"`
+	International_phone_number string    `json:"international_phone_number"`
+	PasswordChangedAt          time.Time `json:"password_changed_at"`
+	CreatedAt                  time.Time `json:"created_at"`
 }
 
 func newUserResponse(user db.User) userResponse {
 	return userResponse{
-		Username:          user.Username,
-		FullName:          user.FullName,
+		Username:                   user.Username,
+		FullName:                   user.FullName,
 		International_phone_number: user.InternationalPhoneNumber,
-		PasswordChangedAt: user.PasswordChangedAt,
-		CreatedAt:         user.CreatedAt,
+		PasswordChangedAt:          user.PasswordChangedAt,
+		CreatedAt:                  user.CreatedAt,
 	}
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
 	var req createUserRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
+		logrus.WithError(err).Error("Failed to bind JSON for createUser")
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	hashedPassword, err := util.HashPassword(req.Password)
 	if err != nil {
+		logrus.WithError(err).Error("Failed to hash password")
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
 	arg := db.CreateUserParams{
-		Username:       req.Username,
-		HashedPassword: hashedPassword,
-		FullName:       req.FullName,
+		Username:                 req.Username,
+		HashedPassword:           hashedPassword,
+		FullName:                 req.FullName,
 		InternationalPhoneNumber: req.International_phone_number,
 	}
 
 	user, err := server.store.CreateUser(ctx, arg)
 	if err != nil {
+		logrus.WithError(err).Error("Failed to create user in database")
 		if db.ErrorCode(err) == db.UniqueViolation {
 			ctx.JSON(http.StatusForbidden, errorResponse(err))
 			return
@@ -65,88 +73,66 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
+	logrus.WithField("username", user.Username).Info("User created successfully")
 	rsp := newUserResponse(user)
 	ctx.JSON(http.StatusOK, rsp)
 }
 
-// type loginUserRequest struct {
-// 	Username string `json:"username" binding:"required,alphanum"`
-// 	Password string `json:"password" binding:"required,min=6"`
-// }
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
 
-// type loginUserResponse struct {
-// 	SessionID             uuid.UUID    `json:"session_id"`
-// 	AccessToken           string       `json:"access_token"`
-// 	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
-// 	RefreshToken          string       `json:"refresh_token"`
-// 	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
-// 	User                  userResponse `json:"user"`
-// }
+type loginUserResponse struct {
+	SessionID            uuid.UUID `json:"session_id"`
+	AccessToken          string    `json:"access_token"`
+	AccessTokenExpiresAt time.Time `json:"access_token_expires_at"`
+	// RefreshToken          string       `json:"refresh_token"`
+	// RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	User userResponse `json:"user"`
+}
 
-// func (server *Server) loginUser(ctx *gin.Context) {
-// 	var req loginUserRequest
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		logrus.WithError(err).Error("Failed to bind JSON for loginUser")
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-// 	user, err := server.store.GetUser(ctx, req.Username)
-// 	if err != nil {
-// 		if errors.Is(err, db.ErrRecordNotFound) {
-// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-// 			return
-// 		}
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		logrus.WithError(err).Error("Failed to get user from database")
+		if errors.Is(err, db.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	err = util.CheckPassword(req.Password, user.HashedPassword)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
-// 		return
-// 	}
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		logrus.WithError(err).Error("Password check failed")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
 
-// 	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
-// 		user.Username,
-// 		user.Role,
-// 		server.config.AccessTokenDuration,
-// 	)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		logrus.WithError(err).Error("Failed to create access token")
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
-// 		user.Username,
-// 		user.Role,
-// 		server.config.RefreshTokenDuration,
-// 	)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
-
-// 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
-// 		ID:           refreshPayload.ID,
-// 		Username:     user.Username,
-// 		RefreshToken: refreshToken,
-// 		UserAgent:    ctx.Request.UserAgent(),
-// 		ClientIp:     ctx.ClientIP(),
-// 		IsBlocked:    false,
-// 		ExpiresAt:    refreshPayload.ExpiredAt,
-// 	})
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
-
-// 	rsp := loginUserResponse{
-// 		SessionID:             session.ID,
-// 		AccessToken:           accessToken,
-// 		AccessTokenExpiresAt:  accessPayload.ExpiredAt,
-// 		RefreshToken:          refreshToken,
-// 		RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
-// 		User:                  newUserResponse(user),
-// 	}
-// 	ctx.JSON(http.StatusOK, rsp)
-// }
+	logrus.WithField("username", user.Username).Info("User logged in successfully")
+	rsp := loginUserResponse{
+		AccessToken:          accessToken,
+		AccessTokenExpiresAt: accessPayload.ExpiredAt,
+		User:                 newUserResponse(user),
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}
