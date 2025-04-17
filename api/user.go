01cb34b969c6ec5d25e554e0da678e471fcb57cd
net-cyber <devnatnaels@gmail.com>
2025-04-18 01:26:17 +0300

    Update userResponse struct to include Token field
    - Added a Token field to the userResponse struct for enhanced user data representation.
    - Updated the newUserResponse function to populate the Token field from the user model.

--- api/user.go
+++ api/user.go
@@ -28,6 +28,7 @@ type userResponse struct {
 	FullName                   string    `json:"full_name"`
 	International_phone_number string    `json:"international_phone_number"`
 	Avatar                     string    `json:"avatar"`
+	Token                      string    `json:"token"`
 	PhoneVerified              bool      `json:"phone_verified"`
 	PasswordChangedAt          time.Time `json:"password_changed_at"`
 	CreatedAt                  time.Time `json:"created_at"`
@@ -39,6 +40,7 @@ func newUserResponse(user db.User) userResponse {
 		FullName:                   user.FullName,
 		International_phone_number: user.InternationalPhoneNumber,
 		Avatar:                     user.Avatar,
+		Token:                      user.Token,
 		PhoneVerified:              user.PhoneVerified,
 		PasswordChangedAt:          user.PasswordChangedAt,
 		CreatedAt:                  user.CreatedAt,
