651ecebd9fe20cae349e9f9268107cc3f8db3262
net-cyber <devnatnaels@gmail.com>
2025-04-18 17:29:42 +0300

    Add conditional creation for financial institution type and table
    - Modified the SQL migration to check for the existence of the financial institution type before creating it.
    - Updated the financial institutions table creation to ensure it only creates if it does not already exist.
    - Added conditional index creation for the financial institutions table to enhance performance and avoid errors during migration.

--- db/migration/000006_add_financial_institutions.up.sql
+++ db/migration/000006_add_financial_institutions.up.sql
@@ -1,6 +1,11 @@
-CREATE TYPE financial_institution_type AS ENUM ('bank', 'wallet', 'mfi');
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'financial_institution_type') THEN
+        CREATE TYPE financial_institution_type AS ENUM ('bank', 'wallet', 'mfi');
+    END IF;
+END $$;
 
-CREATE TABLE "financial_institutions" (
+CREATE TABLE IF NOT EXISTS "financial_institutions" (
   "id" bigserial PRIMARY KEY,
   "name" varchar NOT NULL,
   "type" financial_institution_type NOT NULL,
@@ -10,4 +15,4 @@ CREATE TABLE "financial_institutions" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE INDEX ON "financial_institutions" ("type");
+CREATE INDEX IF NOT EXISTS financial_institutions_type_idx ON "financial_institutions" ("type");
