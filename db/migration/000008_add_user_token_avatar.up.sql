454ea47ef156c3fa8243bb78973e40f1da70d1c2
net-cyber <devnatnaels@gmail.com>
2025-04-18 10:21:48 +0300

    Update user table schema to set default values for token, avatar, and fcmtoken fields

--- db/migration/000008_add_user_token_avatar.up.sql
+++ db/migration/000008_add_user_token_avatar.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "users" ADD COLUMN "token" varchar NOT NULL;
-ALTER TABLE "users" ADD COLUMN "avatar" varchar NOT NULL;
-ALTER TABLE "users" ADD COLUMN "fcmtoken" varchar NOT NULL;
+ALTER TABLE "users" ADD COLUMN "token" varchar NOT NULL DEFAULT '';
+ALTER TABLE "users" ADD COLUMN "avatar" varchar NOT NULL DEFAULT '';
+ALTER TABLE "users" ADD COLUMN "fcmtoken" varchar NOT NULL DEFAULT '';
 ALTER TABLE "users" ADD COLUMN "online" boolean NOT NULL DEFAULT false;
